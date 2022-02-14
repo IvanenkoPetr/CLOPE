bf389af8d91fa6cac4d77551859ace15391bde9e
IvanenkoPetr <peter98765@rambler.ru>
2022-02-14 22:59:57 +0300

    fix

---
+++ CLOPE.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace CLOPE.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+
+        [Fact]
+        public void Test3()
+        {
+
+        }
+
+
+        [Fact]
+        public void Test4()
+        {
+
+        }
+    }
+}
